4e3cf0b1b23934c7d7b2ea63ebcd615430a5bdfc
Azuro <84352029+Azuro467@users.noreply.github.com>
2021-08-16 20:43:33 +1200

    Test4

---
+++ scarymazegame/Assets/Scripts/MonsterGeneration.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterGeneration : MonoBehaviour
+{
+    public GameObject arm;
+
+    public int armCount;
+
+    float RandomRange(float min, float max)
+    {
+        return Random.Range(min, max);
+    }
+
+    int FindArmPoints()
+    {
+        return (gameObject.transform.Find("ArmPoints").transform.childCount);
+    }
+
+    void SpawnArms()
+    {
+        for (int i = 0; i < armCount; i++)
+        {
+            //spawn arm
+            GameObject newArm = Instantiate(arm);
+            newArm.transform.SetParent(gameObject.transform.Find("Arms"));
+            print("arm " + i + " spawned");
+
+            //pos and rot arm arm
+            newArm.transform.localPosition = gameObject.transform.Find("ArmPoints").transform.GetChild(i).transform.localPosition;
+            newArm.transform.eulerAngles = gameObject.transform.Find("ArmPoints").transform.GetChild(i).transform.eulerAngles;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Update()
+    {        
+        //how many arms spawn
+        armCount = 5;//(int) RandomRange(1f, FindArmPoints());
+
+        //spawn arms
+        SpawnArms();
+
+    }
+}
