c937f0039b9f020f1c3ba38f6f4cff8ff6d5ba98
OliTheVamp <88023081+OliTheVamp@users.noreply.github.com>
2021-08-30 16:28:34 +1200

    Setting and General UI
    This is the current work on all the UI I have done. Future changes will happen.  Inventory system and its UI will come soon.

---
+++ scarymazegame/Assets/Scripts/ConnerScripts/PauseMenu.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Canvas;
+    public GameObject Panel;
+    public bool m_GamePaused = false;
+    MouseLook MainCamera;
+    void Start()
+    {
+       // Panel.SetActive(false);
+        m_GamePaused = false;
+        MainCamera = FindObjectOfType<MouseLook>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (m_GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
+    }
+
+
+
+    public void Resume()
+    {
+        Canvas.SetActive(false);
+        Time.timeScale = 1.0f;
+        m_GamePaused = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        MainCamera.m_Sensitivity = 0.5f;
+    }
+
+    public void Pause()
+    {
+        Canvas.SetActive(true);
+        Time.timeScale = 0.0f;
+        m_GamePaused = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        MainCamera.m_Sensitivity = 0;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit!!!");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Options()
+    {
+        Panel.SetActive(true);
+        Canvas.SetActive(false);
+    }
+
+    public void Return()
+    {
+        Panel.SetActive(false);
+        Canvas.SetActive(true);
+    }
+}
