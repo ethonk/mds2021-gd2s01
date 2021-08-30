b14ffee04fa1b3a9b1fa1da4ce6b7adfbf70a11f
ethonk <62923028+ethonk@users.noreply.github.com>
2021-08-30 16:47:05 +1200

    Prefab Fixes

---
+++ scarymazegame/Assets/Scripts/EthanScripts/MonsterMovement/EnemyFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        enemy.SetDestination(player.position);
+    }
+}
