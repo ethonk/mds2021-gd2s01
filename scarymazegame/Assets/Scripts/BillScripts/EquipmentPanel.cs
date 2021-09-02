8b66fc0134222dda1d0f7b69f726ec28ee703c28
Cycrone <Cycronebro@gmail.com>
2021-09-02 21:42:14 +1200

    Item and inventory
    start of equipping item and inventory access code. Currently can add items onto inv (manually via script) and can rigth click equip onto character assigned slot. Talk to me if u wanna kno

---
+++ scarymazegame/Assets/Scripts/BillScripts/EquipmentPanel.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EquipmentPanel : MonoBehaviour
+{
+    [SerializeField] Transform equipmentSlotsParent;
+    [SerializeField] EquipmentSlot[] equipmentSlots;
+
+    private void OnValidate()
+    {
+        equipmentSlots = equipmentSlotsParent.GetComponentsInChildren<EquipmentSlot>();
+    }
+
+    public bool AddItem(EquippableItems item, out EquippableItems previousItem)
+    {
+        for (int i = 0; i < equipmentSlots.Length; i++)
+        {
+            if (equipmentSlots[i].EquipmentType == item.EquipmentType)
+            {
+                previousItem = (EquippableItems)equipmentSlots[i].Item;
+                equipmentSlots[i].Item = item;
+                return true;
+            }
+        }
+        previousItem = null;
+        return false;
+    }
+
+    public bool RemoveItem(EquippableItems item)
+    {
+        for (int i = 0; i < equipmentSlots.Length; i++)
+        {
+            if (equipmentSlots[i].Item == item)
+            {
+                equipmentSlots[i].Item = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ scarymazegame/Assets/Scripts/BillScripts/Inventory.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] List<Item> items;
+    [SerializeField] Transform itemsParent;
+    [SerializeField] ItemSlot[] itemSlots;
+
+    public event Action<Item> OnItemRightClickEvent;
+
+    private void Awake()
+    {
+        for (int i = 0; i < itemSlots.Length; i++)
+        {
+            itemSlots[i].OnRightClickEvent += OnItemRightClickEvent;
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (itemsParent != null)
+            itemSlots = itemsParent.GetComponentsInChildren<ItemSlot>();
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        int i = 0;
+        for (; i < items.Count && i < itemSlots.Length; i++)        //Every item we have this will assign it to an item slot
+        {
+            itemSlots[i].Item = items[i];
+        }
+
+        for (; i < itemSlots.Length; i++)                           //For every item slot that is empty this will set the item to null
+        {
+            itemSlots[i].Item = null;
+        }
+    }
+
+    public bool AddItem(Item item)                                  //Adding item check full or not
+    {
+        if (IsFull())
+        {
+            return false;
+        }
+        else 
+        {
+            items.Add(item);
+            RefreshUI();
+            return true;
+        }
+    }
+
+    public bool RemoveItem(Item item)                               //Removing items
+    {
+        if (items.Remove(item))
+        {
+            RefreshUI();
+            return true;
+        }
+        return false;
+    }
+
+    public bool IsFull()                                        //Checks if inv is full by counting if there are more or equal amounts items than the itemSlots
+    {
+        return items.Count >= itemSlots.Length;
+
+    }
+
+}
